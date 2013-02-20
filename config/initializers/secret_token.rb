113fa8b4695a0cbb57c874f7a5585db285a99195
Eduardo Raad <eduardoraad@gmail.com>
2013-02-20 02:56:21 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bolondron::Application.config.secret_token = '98b71df0c18bac983fb7566f8de8fe7d3f71a657f7bcedd1ab345efea5f4a275d1451e17b63923ad4a6a3c7c81cf311155c91c78ba85f62d527f0b27b207d1e4'
